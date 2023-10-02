cfc06923d14b136036383dbe121a1766521f6113
Renz Torres <104774186+scrubgay@users.noreply.github.com>
2023-10-02 10:40:40 -0400

    add batch logic

--- scraper.py
+++ scraper.py
@@ -4,18 +4,40 @@ import json
 
 logging.basicConfig(level=logging.DEBUG)
 
+# i fuck it
+# https://stackoverflow.com/questions/434287/how-to-iterate-over-a-list-in-chunks
+def chunker(seq, size):
+    return (seq[pos:pos + size] for pos in range(0, len(seq), size))
+
 def scrape(category:str, output:str):
     # LEVEL 1 - scrape schedules from calendars
-    # argument days is the number of day start from today
-    calendar_url_list = scraper.get_calendar_list(category, days=0, days_out = 365*5)
+    calendar_url_list = scraper.get_calendar_list(category, days=-365*10, days_out = 365*5)
     box_url_list = scraper.get_box_list(calendar_url_list)
-    # LEVEL 2 - scrape the real data
-    data = scraper.get_data(box_url_list)
-    # save data
-    with open(output, 'w') as fout:
-         json.dump(data, fout)
-         logging.info(f"Data saved to {output}")
+
+    # batch/chunk so it doesn't crash on big inputs
+    box_chunks = [chunk for chunk in chunker(box_url_list, 20)]
+
+    errors = []
+    # do in batches to hedge against fuck shit
+    for i in range(len(box_chunks)) :
+        # LEVEL 2 - scrape the real data
+        data, error = scraper.get_data(box_chunks[i])
+        errors.append(error)
+        output_i = output + str(i) + ".json"
+        # save data
+        with open(output_i, 'w') as fout:
+            json.dump(data, fout)
+            logging.info(f"{category} data batch {i} saved to {output_i}")
+    
+    error_output_name = output + "_errors.json"
+    print("These dates had errors:")
+    print(errors)
+    errors = sum(errors, [])
+    with open(error_output_name, "w") as f :
+        json.dump(errors, f)
+        logging.info(f"Saved errors to {error_output_name}")
+    
 
 if __name__ == '__main__':
-    scrape('foreclose', 'history/foreclose.json')
-    scrape('taxdeed', 'history/taxdeed.json')
+    scrape('foreclose', 'history/foreclose')
+    scrape('taxdeed', 'history/taxdeed')
